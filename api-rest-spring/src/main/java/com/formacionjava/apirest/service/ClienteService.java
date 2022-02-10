2379fc49dc9e88e839165863b088bfc5ae9a8fef
judithandres <judithandres2000@gmail.com>
2022-02-10 11:33:07 +0100

    PRIMER COMMIT SUBIDA DE ARCHIVOS

---
+++ api-rest-spring/src/main/java/com/formacionjava/apirest/service/ClienteService.java
@@ -0,0 +1,16 @@
+package com.formacionjava.apirest.service;
+import com.formacionjava.apirest.entity.Cliente;
+
+import java.util.List;
+
+public interface ClienteService {
+
+	public List<Cliente> findAll();
+	
+	public Cliente findById(Long id);
+	
+	public Cliente save(Cliente cliente);
+	
+	public void delete (Long id);
+	
+}
